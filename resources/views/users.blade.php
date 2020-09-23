c5378dce694371ec124822eeaf4696372611ec39
Luis Antonio Parrado <luisprmat@gmail.com>
2020-09-23 01:49:48 -0500

    Update composer dependencies

---
+++ resources/views/users.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h1 class="font-semibold text-xl text-gray-800 leading-tight">Listado de usuarios</h1>
+                <ul>
+                    @forelse (App\Models\User::all() as $user)
+                        <li>{{ $user->name }}: <i>({{ $user->email }})</i></li>
+                    @empty
+                        <li>No hay usuarios</li>
+                    @endforelse
+                </ul>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
